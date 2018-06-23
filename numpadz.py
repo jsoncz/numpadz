6163865610abc59dd2a75c46b0f91839aaa7b55c
Romain Richard <romain.richard.IT.engineer@gmail.com>
2018-06-22 18:25:48 -0700

    Basic refactoring and cleanup
    - Replaced tabs with 4 spaces
    - Replaced a, b, c, ... variables with the sounds variable
    - Factorized the handling of key presses

--- numpadz.py
+++ numpadz.py
@@ -7,13 +7,13 @@ speed = [2, 2]
 colour = 0, 0, 0
 
 try:
-	packs = []
-	packs = [line.rstrip('\n') for line in open('wav/packlist.txt')]
-	print ("NUMPADZ - Cycle through Packs with + / -")
-	print (packs)
+    packs = []
+    packs = [line.rstrip('\n') for line in open('wav/packlist.txt')]
+    print ("NUMPADZ - Cycle through Packs with + / -")
+    print (packs)
 
 except:
-	print ("could not open packlist, check you run from the current dir")
+    print ("could not open packlist, check you run from the current dir")
 
 
 selectedPack = 0
@@ -22,123 +22,47 @@ p = packs[selectedPack]
 
 screen = pygame.display.set_mode(size)
 
-a = pygame.mixer.Sound("wav/"+p+"/0001.wav")
-b = pygame.mixer.Sound("wav/"+p+"/0002.wav")
-c = pygame.mixer.Sound("wav/"+p+"/0003.wav")
-d = pygame.mixer.Sound("wav/"+p+"/0004.wav")
-e = pygame.mixer.Sound("wav/"+p+"/0005.wav")
-f = pygame.mixer.Sound("wav/"+p+"/0006.wav")
-g = pygame.mixer.Sound("wav/"+p+"/0007.wav")
-h = pygame.mixer.Sound("wav/"+p+"/0008.wav")
-i = pygame.mixer.Sound("wav/"+p+"/0009.wav")
+sounds = [pygame.mixer.Sound("wav/{}/{:04}.wav".format(p, i))
+          for i in range(1, 10)]
+
+KEYS = [pygame.K_KP1, pygame.K_KP2, pygame.K_KP3, pygame.K_KP4, pygame.K_KP5,
+        pygame.K_KP6, pygame.K_KP7, pygame.K_KP8, pygame.K_KP9]
+
 while 1:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
-		#choose pack
-		if event.type == pygame.KEYDOWN:
-			colour
-			if event.key == pygame.K_KP_PLUS:
-				try:
-					selectedPack += 1
-					p = packs[selectedPack]
-					print(p)
-				except IndexError:
-					selectedPack = 0
-					p = packs[selectedPack]
-					print(p)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+        #choose pack
+        if event.type == pygame.KEYDOWN:
+            colour
+            if event.key == pygame.K_KP_PLUS:
+                selectedPack += 1
+            elif event.key == pygame.K_KP_MINUS:
+                selectedPack -= 1
 
-				a = pygame.mixer.Sound("wav/"+p+"/0001.wav")
-				b = pygame.mixer.Sound("wav/"+p+"/0002.wav")
-				c = pygame.mixer.Sound("wav/"+p+"/0003.wav")
-				d = pygame.mixer.Sound("wav/"+p+"/0004.wav")
-				e = pygame.mixer.Sound("wav/"+p+"/0005.wav")
-				f = pygame.mixer.Sound("wav/"+p+"/0006.wav")
-				g = pygame.mixer.Sound("wav/"+p+"/0007.wav")
-				h = pygame.mixer.Sound("wav/"+p+"/0008.wav")
-				i = pygame.mixer.Sound("wav/"+p+"/0009.wav")
-			elif event.key == pygame.K_KP_MINUS:
-				try:
-					selectedPack -= 1
-					p = packs[selectedPack]
-					print(p)
-				except IndexError:
-					selectedPack = 0
-					p = packs[selectedPack]
-					print(p)
+            try:
+                p = packs[selectedPack]
+            except IndexError:
+                selectedPack = 0
+                p = packs[selectedPack]
+            print(p)
 
-				a = pygame.mixer.Sound("wav/"+p+"/0001.wav")
-				b = pygame.mixer.Sound("wav/"+p+"/0002.wav")
-				c = pygame.mixer.Sound("wav/"+p+"/0003.wav")
-				d = pygame.mixer.Sound("wav/"+p+"/0004.wav")
-				e = pygame.mixer.Sound("wav/"+p+"/0005.wav")
-				f = pygame.mixer.Sound("wav/"+p+"/0006.wav")
-				g = pygame.mixer.Sound("wav/"+p+"/0007.wav")
-				h = pygame.mixer.Sound("wav/"+p+"/0008.wav")
-				i = pygame.mixer.Sound("wav/"+p+"/0009.wav")
-		#numpad input
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_KP1:
-				a.play()
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_KP1:
-				a.stop()
+            sounds = [pygame.mixer.Sound("wav/{}/{:04}.wav".format(p, i))
+                      for i in range(1, 10)]
 
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_KP2:
-				b.play()
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_KP2:
-				b.stop()
+        if event.type in (pygame.KEYDOWN, pygame.KEYUP):
+            try:
+                key_index = KEYS.index(event.key)
+            except ValueError:
+                # User didn't press a numpad key, ignore
+                pass
+            else:
+                # User did press a numpad key, process it
+                sound = sounds[key_index]
+                if event.type == pygame.KEYDOWN:
+                    sound.play()
+                elif event.type == pygame.KEYUP:
+                    sound.stop()
 
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_KP3:
-				c.play()
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_KP3:
-				c.stop()
-		
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_KP4:
-				d.play()
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_KP4:
-				d.stop()
-		
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_KP5:
-				e.play()
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_KP5:
-				e.stop()
-		
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_KP6:
-				f.play()
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_KP6:
-				f.stop()
-						
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_KP7:
-				g.play()
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_KP7:
-				g.stop()
-		
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_KP8:
-				h.play()
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_KP8:
-				h.stop()
-				
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_KP9:
-				i.play()
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_KP9:
-				i.stop()
-					
-		screen.fill(colour)
+        screen.fill(colour)
 
-		pygame.display.flip()
+        pygame.display.flip()
