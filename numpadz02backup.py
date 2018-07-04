5f895abf98d698a0f3124e56fe6144c50b098be5
jsoncz <jsoncz@gmail.com>
2018-07-04 02:00:02 +0200

    added some optimzation and logo

--- numpadz02backup.py
+++ numpadz02backup.py
@@ -3,7 +3,7 @@ import sys
 import pygame
 import time
 
-pygame.mixer.pre_init(44100, 16, 16, 2048)
+pygame.mixer.pre_init(44100, 16, 16, 1024)
 pygame.mixer.init()
 
 start = time.time()
@@ -11,21 +11,20 @@ start = time.time()
 SIZE = WIDTH, HEIGHT = 320, 300
 # Define some colors
 BLACK = (0, 0, 0)
-WHITE = (255, 155, 155)
+WHITE = (255, 185, 55)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-speed = 6
-
+speed = 5
 
 POS = 1
 lastpos = []
 key_indexs = []
-recordpack = []
-recordlen = []
-recordkey = []
+recpack = []
+reclen = []
+reckey = []
 record = False
-play = False
+play = True
 clock = pygame.time.Clock()
 
 
@@ -38,31 +37,24 @@ except IOError:
     print("could not open packlist, check you run from the current dir")
     sys.exit()
 
-# GIFLIST
-try:
-    GIFPACKS = []
-    GIFPACKS = [line.rstrip('\n') for line in open('gif/giflist.txt')]
-    print("NUMPADZ - Cycle through GIFS with / and * -")
-    print(GIFPACKS)
-except IOError:
-    print("could not open giflist, check you run from the current dir")
-    pass
 # PACK VARIABLES
 SELECTEDPACK = 0
 SELECTEDGIFPACK = 0
 P = PACKS[SELECTEDPACK]
 P = PACKS[SELECTEDPACK]
-GP = GIFPACKS[SELECTEDGIFPACK]
 
 # WINDOW
 SCREEN = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('NumPADZ')
+SCREEN.set_alpha(None)
+logo = pygame.image.load("logo.png").get_rect()
+logosurface = pygame.Surface((logo.width, logo.height))
+logosurface.blit(pygame.image.load("logo.png"), logo)
+
 
 # FILES
 SOUNDS = [pygame.mixer.Sound("wav/{}/{:04}.wav".format(P, i))
           for i in range(1, 10)]
-GIFS = [pygame.image.load("gif/{}/{:04}.gif".format(GP, i))
-        for i in range(1, 10)]
 
 KEYS = [pygame.K_KP1, pygame.K_KP2, pygame.K_KP3, pygame.K_KP4, pygame.K_KP5,
         pygame.K_KP6, pygame.K_KP7, pygame.K_KP8, pygame.K_KP9]
@@ -72,14 +64,6 @@ def get_sounds(P):
     return [pygame.mixer.Sound("wav/{}/{:04}.wav".format(P, i))
             for i in range(1, 10)]
 
-def get_gifs(GP):
-    return [pygame.image.load("gif/{}/{:04}.gif".format(GP, i))
-            for i in range(1, 10)]
-
-def image(gif):
-    SCREEN.blit(gif, (1, 0))
-
-
 def getsamplelen():
     # GET SAMPLE LENGTHS
     for i in SOUNDS:
@@ -87,9 +71,9 @@ def getsamplelen():
         print("Num", SOUNDS.index(i) + 1, "-", samplelength)
 
 def grid():
-    W=int(20-speed)
-    H=30
-    MARGIN=speed-1
+    W=int(18-speed)
+    H=20
+    MARGIN=15-speed
     SCREEN.fill(BLACK)
     for column in range(5+MARGIN,300,W+MARGIN):
         pygame.draw.rect(SCREEN,WHITE, [column,0+MARGIN,W,H])
@@ -100,10 +84,8 @@ def drawsound(key_index):
     if record or play:
         for i in range(len(lastpos)):
             keypos = key_indexs[i]*20
-            #if lastpos[i]
             try:
-                soundrect = pygame.draw.rect(SCREEN,COLOUR,[lastpos[i],keypos,recordlen[i]*(speed*30),10])
-                print("POSITION",lastpos[i])
+                soundrect = pygame.draw.rect(SCREEN,COLOUR,[lastpos[i],keypos,reclen[i]*(speed*30),10])
             except IndexError:
                 pass
 
@@ -111,16 +93,14 @@ def get_rect(self):
     return pygame.Rect(self.x, self.y, self.width, self.height)
 
 def playrecording():
-    if record or play:
-        for i in range(len(recordpack)):
-                try:
-                    recsnd = pygame.mixer.Sound("wav/{}/{:04}.wav".format(recordpack[i], recordkey[i]))
-                    if POS == lastpos[i]:
-                        ms = int(recordlen[i]*1000)
-                        recsnd.play(-1,maxtime=ms)
-                   
-                except IndexError:
-                    pass
+    for i in range(len(recpack)):
+        try:
+            recsnd = pygame.mixer.Sound("wav/{}/{:04}.wav".format(recpack[i], reckey[i]))
+            if POS == lastpos[i]:
+                ms = int(reclen[i]*1000)
+                recsnd.play(-1,maxtime=ms)
+        except IndexError:
+            pass
 
 while 1:
     if record:
@@ -128,14 +108,13 @@ while 1:
     else:
         COLOUR = GREEN
     POS+=speed
-    clock.tick(30)
+    clock.tick(25)
 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.KEYDOWN:
-
-              # SET RECORD ON/OFF
+            # SET RECORD ON/OFF
             if event.key == pygame.K_NUMLOCK:
                 if record:
                     record = False
@@ -143,6 +122,7 @@ while 1:
                     pygame.mixer.stop()
                 else:
                     record = True
+                    play = True
                     print ("STARTED RECORDING")
 
 
@@ -150,45 +130,45 @@ while 1:
             if event.key == pygame.K_KP0:
                 print("stopping all SOUNDS with Num Zero")
                 pygame.mixer.quit()
-                pygame.mixer.pre_init(44100, 16, 16, 2048)
+                pygame.mixer.pre_init(44100, 16, 16, 1024)
                 pygame.mixer.init()
 
             if event.key == pygame.K_KP_MULTIPLY:
                 try:
                     del lastpos[-1]
                     del key_indexs[-1]
-                    del recordpack[-1]
-                    del recordlen[-1]
-                    del recordkey[-1]
+                    del recpack[-1]
+                    del reclen[-1]
+                    del reckey[-1]
                 except IndexError:
                     pass
 
             #SPEED CONTROL
             if event.key == pygame.K_PAGEUP:
-                if speed <= 9:
-                    speed+=2                   
+                if speed <= 5:
+                    speed+=1                   
                 else:
                     print("Speed is at maximum:",speed)
                 print("Speed:",speed)
             if event.key == pygame.K_PAGEDOWN:
-                if speed >= 4:
-                    speed-=2
+                if speed >= 3:
+                    speed-=1
                 else:
                     print("Speed is at minimum:",speed)
                 print("Speed:",speed)
+
             #SAMPLE HOLD
             if event.key == pygame.K_KP_ENTER:
                 SOUNDS = get_sounds(P)
-            # SELECT GIFPACK WITH * AND /
 
             #CLEAR RECORDING
             if event.key == pygame.K_KP_PERIOD:
                 try:
                     lastpos = []
                     key_indexs = []
-                    recordpack = []
-                    recordlen = []
-                    recordkey = []
+                    recpack = []
+                    reclen = []
+                    reckey = []
                 except IndexError:
                     pass
             #PLAYBACK MODE
@@ -231,38 +211,33 @@ while 1:
             else:
                 # User did press a numpad key, process it
                 sound = SOUNDS[key_index]
-                gifSel = GIFS[key_index]
                 if event.type == pygame.KEYDOWN:
                     curpos = POS
                     if record:
                         lastpos.append(curpos)
                         key_indexs.append(key_index)
-                        #samplelength = round(sound.get_length(), 1)2
                         smptimepos = time.time()
-                        smpstarted = smptimepos - start 
-                        #print(P, key_index+1, smptimepos)
+                        smpstarted = smptimepos - start
                     sound.play(-1)
                     smpstart = time.time()
-
 
                 if event.type == pygame.KEYUP:
                     sound.stop()
                     smpstop = time.time()
-                    print(curpos,P,key_index+1,round(smpstop - smpstart,2))
                     if record:
-                        recordpack.append(P)
-                        recordlen.append(round(smpstop - smpstart,2))
-                        recordkey.append(key_index+1)
-                    
+                        recpack.append(P)
+                        reclen.append(round(smpstop - smpstart,2))
+                        reckey.append(key_index+1)
+    pygame.event.pump()
     grid()
     if POS <= WIDTH:
         curpos = POS
-        #print(pos)
         pygame.draw.rect(SCREEN, COLOUR, [curpos,3,5,180])
-        #pygame.display.flip()
     else:
-        POS = 1
-
+        POS = 0
     drawsound(key_indexs)
-    playrecording()
+    if play:
+        playrecording()
+    #SCREEN.blit(logo, (1,170))
+    SCREEN.blit(logosurface, (2,170))
     pygame.display.flip()
